5d93733ffe9abd01beaf291d17b1d928e88b62cc
Shailesh Maddhesiya <shaileshmadde650@gmail.com>
2022-03-20 19:58:59 +0530

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+
+  final int ans=30;
+  final String name="codepur";
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("welcome to $ans of $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
